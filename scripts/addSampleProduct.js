a31a464eb9eff962852cf9381b06eb84fb1aefd7
Petrus Borges Machado <contatopetrusborges@gmail.com>
2026-02-13 23:47:57 -0300

    Update addSampleProduct.js

--- scripts/addSampleProduct.js
+++ scripts/addSampleProduct.js
@@ -1,10 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
   const addSampleProductButton = document.querySelector(".add-sample-button");
+  let productId = 4;
 
   addSampleProductButton.addEventListener("click", () => {
     const gridCards = document.querySelector(".product-grid");
     const newProductArticle = document.createElement("article");
+
     newProductArticle.classList.add("card");
+    newProductArticle.dataset.id = productId++;
 
     newProductArticle.innerHTML = `
       <div class="card-image">
